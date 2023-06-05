0ea81c08cc07bae8f1d96b55d9ce2f16a02160f2
xiaonanfu-ucsd <xif003@ucsd.edu>
2023-06-05 00:03:51 -0700

    part1

--- assets/scripts/speechSynth.js
+++ assets/scripts/speechSynth.js
@@ -8,7 +8,10 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   setTimeout(() => populateVoices(), 50);
   bindListeners();
+  featureFlag();
 }
+
+let isVoicesReady = false;
 
 function populateVoices() {
   const voiceSelect = document.querySelector('#voice-select');
@@ -20,6 +23,52 @@ function populateVoices() {
     option.setAttribute('data-index', voiceSelect.children.length - 1)
     voiceSelect.appendChild(option);
   });
+    isVoicesReady = true;
+}
+
+function reduceVoices() {
+    const voiceSelect = document.querySelector('#voice-select');
+    let index = 0;
+    while (index < voiceSelect.children.length) {
+        if (!voiceSelect.children[index].textContent.includes('(en-US')) {
+            voiceSelect.removeChild(voiceSelect.children[index]);
+            index--;
+        }
+        index++;
+    }
+}
+
+function featureFlag() {
+    const clientSideID = '647c4327830765131d495237';
+    const flagKey = 'reduce-voices';
+    const context = {
+      kind: 'user',
+      key: Math.random().toString(36).substring(7),
+    };
+
+    const ldclient = LDClient.initialize(clientSideID, context);
+
+    async function render() {
+        console.log('render');
+        const flagValue = ldclient.variation(flagKey, true);
+        console.log(flagValue);
+        if (flagValue) {
+            while (!isVoicesReady) {
+                await new Promise(resolve => setTimeout(resolve, 50));
+            }
+            reduceVoices();
+        }
+    }
+
+    ldclient.on('initialized', () => {
+        console.log('SDK successfully initialized!');
+    });
+    ldclient.on('failed', () => {
+        console.log('SDK failed to initialize');
+    });
+    ldclient.on('ready', render);
+    ldclient.on('change', render);
+    ldclient.close();
 }
 
 function bindListeners() {
